9b4bc0aa0a9625fb67b81ff081218dd8b7c34469
merlynar09 <148832483+merlynar09@users.noreply.github.com>
2023-10-24 11:17:17 -0500

    cambio
    Puse mayuscula

--- base/php/conexion.php
+++ base/php/conexion.php
@@ -5,7 +5,7 @@ if ($conexion)
 {
     echo 'conectado exitosamente a la base de datos';
 }else{
-    echo 'no se ha podido conectar a la base de datos';
+    echo 'No se ha podido conectar a la base de datos';
 }
 
 ?>
